cf171ee01c8cdc0fe5287a2fb7e12de24de0cc2d
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-10 20:52:42 -0300

    ensure insert data if data is provided

--- src/infra/repo/space_flight_new_repository.py
+++ src/infra/repo/space_flight_new_repository.py
@@ -1,9 +1,19 @@
 from typing import Dict
 from src.infra.interfaces import SpaceFlightNewInterfaceRepository
 from src.utils.errors import MissingParamError
+from src.infra.config import DBConnectionHandler
 
 
 class SpaceFlightNewRepository(SpaceFlightNewInterfaceRepository):
     def insert(self, data: Dict = None) -> Dict:
         if not data:
             raise MissingParamError("data")
+
+        try:
+            with DBConnectionHandler() as connection:
+                collection = connection.get_collection("spaceFlight")
+                inserted_space_flight = collection.insert_one(data)
+
+            return inserted_space_flight
+        except:
+            raise Exception("server error")
--- src/infra/repo/space_flight_new_repository_test.py
+++ src/infra/repo/space_flight_new_repository_test.py
@@ -1,6 +1,15 @@
 import pytest
+from src.infra.test import mock_one_space_flight
 from .space_flight_new_repository import SpaceFlightNewRepository
+from src.infra.config import DBConnectionHandler
 from src.utils.errors import MissingParamError
+
+
+def test_should_insert_if_data_is_provided():
+    respository = SpaceFlightNewRepository()
+    response = respository.insert(mock_one_space_flight)
+
+    assert response.inserted_id is not None
 
 
 def test_should_raise_if_no_data_is_privided():
@@ -10,3 +19,10 @@ def test_should_raise_if_no_data_is_privided():
         sut.insert()
 
     assert str(exe_info.value) == str(MissingParamError("data"))
+
+
+@pytest.fixture(scope="module", autouse=True)
+def my_fixture():
+    yield
+    with DBConnectionHandler() as connection:
+        connection.get_collection("spaceFlight").delete_many({})
