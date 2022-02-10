091226510e4342fb95cc68cc14730ec0e497a1f6
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-10 20:50:26 -0300

    create mock to space flight

---
+++ src/infra/test/mocks.py
@@ -0,0 +1,16 @@
+mock_one_space_flight = {
+    "id": 12121,
+    "title": "New Sun Missions to Help NASA Better Understand Earth-Sun Environment",
+    "url": "http://www.nasa.gov/press-release/new-sun-missions-to-help-nasa-better-understand-earth-sun-environment",
+    "imageUrl": "https://www.nasa.gov/sites/default/files/thumbnails/image/oct_1_2015_flare_02.jpg?itok=TXuDcXVl",
+    "newsSite": "NASA",
+    "summary": """NASA has selected two science missions – the Multi-slit
+    Solar Explorer (MUSE) and HelioSwarm – to help improve our
+    understanding of the dynamics of the Sun, the Sun-Earth
+    connection, and the constantly changing space environment.""",
+    "publishedAt": "2022-02-10T21:29:00.000Z",
+    "updatedAt": "2022-02-10T21:29:49.711Z",
+    "featured": False,
+    "launches": [],
+    "events": [],
+}
