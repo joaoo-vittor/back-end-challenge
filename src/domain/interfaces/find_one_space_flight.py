dd4580cc3249a75702e2e0e486fbf49dc0e1d6eb
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-12 12:07:44 -0300

    improve interface

--- src/domain/interfaces/find_one_space_flight.py
+++ src/domain/interfaces/find_one_space_flight.py
@@ -6,5 +6,5 @@ class FindOneSpaceFlightUseCaseInterface(ABC):
     """Find One Space Flight UseCase"""
 
     @abstractmethod
-    def find_one_by(self, id: int) -> Dict:
+    def find_one_by(self, id: int = None) -> Dict:
         raise Exception("Implements method: find one by id")
