aa62db84a3397f50aa7154b8c9c4ba166343ebad
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-13 18:24:41 -0300

    implementing route to get articles

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, Request as RequestFastApi
 from fastapi.responses import JSONResponse
-from src.main.compose import insert_space_flight_compose
-from src.main.adapters import request_adapter
+from src.main.compose import insert_space_flight_compose, find_space_flight_compose
+from src.main.adapters import request_adapter, request_adapter_query_params
 from src.presenters.errors import HttpErrors
+from json import loads, dumps
 
 routes = APIRouter()
+
+
+def parse_json(data: dict):
+    return loads(dumps(data))
 
 
 @routes.get("/")
@@ -14,7 +19,7 @@ def index(request: RequestFastApi):
     )
 
 
-@routes.post("/articles/")
+@routes.post("/articles")
 async def index(request: RequestFastApi):
     controller = insert_space_flight_compose()
     response = None
@@ -27,4 +32,19 @@ async def index(request: RequestFastApi):
     return JSONResponse(
         status_code=response.status_code,
         content={"inserted_id": str(response.body.inserted_id)},
+    )
+
+
+@routes.get("/articles")
+async def index(request: RequestFastApi):
+    controller = find_space_flight_compose()
+    response = None
+    try:
+        response = request_adapter_query_params(request, controller.route)
+    except Exception as e:
+        response = HttpErrors.error_500()
+
+    return JSONResponse(
+        status_code=response.status_code,
+        content=response.body,
     )
