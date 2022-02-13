f7afb0f5f766a5019fd16daeb7900375438592cf
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-13 13:33:55 -0300

    impelmenting and test FindSpaceFlight controller

---
+++ src/presenters/controllers/find_space_flight/find_space_flight.py
@@ -0,0 +1,46 @@
+from typing import Type
+from src.main.interface import RouterInterface
+from src.domain.interfaces.find_space_flight import (
+    FindSpaceFlightUseCaseInterface,
+)
+from src.presenters.helpers import HttpRequest, HttpResponse
+from src.presenters.errors import HttpErrors
+from src.utils.validators import validate_limit_and_skip
+
+
+class FindSpaceFlightController(RouterInterface):
+    def __init__(
+        self, find_space_flight_use_case: Type[FindSpaceFlightUseCaseInterface]
+    ) -> None:
+        self.find_space_flight_use_case = find_space_flight_use_case
+
+    def route(self, http_request: Type[HttpRequest]) -> HttpResponse:
+
+        response = None
+
+        if http_request.body:
+
+            body_params_is_valid = validate_limit_and_skip(http_request.body)
+
+            if body_params_is_valid:
+                limit_page = http_request.body["limit_page"]
+                skip_page = http_request.body["skip_page"]
+                response = self.find_space_flight_use_case.find_by(
+                    limit_page=limit_page, skip_page=skip_page
+                )
+
+            else:
+                response = {"success": False, "data": None}
+
+            if response["success"] is False:
+                http_error = HttpErrors.error_422()
+                return HttpResponse(
+                    status_code=response["status_code"], body=response["body"]
+                )
+
+            return HttpResponse(status_code=200, body=response["data"])
+
+        http_error = HttpErrors.error_400()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
---
+++ src/presenters/controllers/find_space_flight/find_space_flight_test.py
@@ -0,0 +1,39 @@
+from src.domain.test.find_space_flight_spy import FindSpaceFlightUseCaseSpy
+from src.infra.test import SpaceFlightNewRepositorySpy
+from src.presenters.helpers import HttpRequest
+from .find_space_flight import FindSpaceFlightController
+
+
+def make_sut():
+    use_case = FindSpaceFlightUseCaseSpy(SpaceFlightNewRepositorySpy())
+    sut = FindSpaceFlightController(use_case)
+    return sut, use_case
+
+
+def test_route():
+    sut, use_case = make_sut()
+
+    attributes = {"limit_page": 10, "skip_page": 5}
+
+    http_request = HttpRequest(body=attributes)
+
+    http_response = sut.route(http_request=http_request)
+
+    assert attributes["limit_page"] == use_case.params_find_by_limit_page
+    assert attributes["skip_page"] == use_case.params_find_by_skip_page
+
+    assert http_response.status_code == 200
+    assert "error" not in http_response.body
+
+
+def test_route_error_no_body():
+    sut, use_case = make_sut()
+
+    http_request = HttpRequest()
+    http_response = sut.route(http_request=http_request)
+
+    assert use_case.params_find_by_skip_page is None
+    assert use_case.params_find_by_limit_page is None
+
+    assert http_response.status_code == 400
+    assert "error" in http_response.body
