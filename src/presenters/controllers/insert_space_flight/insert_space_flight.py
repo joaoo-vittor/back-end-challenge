d9b9a3c5a147dc203aa15a79e1845ae13f98b742
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-13 14:30:15 -0300

    implementing and test InsertSpaceFlight controller

---
+++ src/presenters/controllers/insert_space_flight/insert_space_flight.py
@@ -0,0 +1,42 @@
+from typing import Type
+from src.main.interface import RouterInterface
+from src.domain.interfaces.insert_space_flight import InsertSpaceFlightUseCaseInterface
+from src.presenters.helpers import HttpRequest, HttpResponse
+from src.presenters.errors import HttpErrors
+from src.utils.validators import validate_data_space_flight
+
+
+class InsertSpaceFlightController(RouterInterface):
+    def __init__(
+        self, insert_space_flight_use_case: Type[InsertSpaceFlightUseCaseInterface]
+    ) -> None:
+        self.insert_space_flight_use_case = insert_space_flight_use_case
+
+    def route(self, http_request: Type[HttpRequest]) -> HttpResponse:
+
+        response = None
+
+        if http_request.body:
+
+            body_params_is_valid = validate_data_space_flight(http_request.body)
+
+            if body_params_is_valid:
+                response = self.insert_space_flight_use_case.insert_one(
+                    data=http_request.body
+                )
+
+            else:
+                response = {"success": False, "data": None}
+
+            if response["success"] is False:
+                http_error = HttpErrors.error_422()
+                return HttpResponse(
+                    status_code=http_error["status_code"], body=http_error["body"]
+                )
+
+            return HttpResponse(status_code=200, body=response["data"])
+
+        http_error = HttpErrors.error_400()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
---
+++ src/presenters/controllers/insert_space_flight/insert_space_flight_test.py
@@ -0,0 +1,38 @@
+from src.domain.test.insert_space_flight_spy import InsertSpaceFlightUseCaseSpy
+from src.infra.test import SpaceFlightNewRepositorySpy, mock_one_space_flight
+from src.presenters.helpers import HttpRequest
+from .insert_space_flight import InsertSpaceFlightController
+
+
+def make_sut():
+    use_case = InsertSpaceFlightUseCaseSpy(SpaceFlightNewRepositorySpy())
+    sut = InsertSpaceFlightController(use_case)
+    return sut, use_case
+
+
+def test_route():
+    sut, use_case = make_sut()
+
+    http_request = HttpRequest(body=mock_one_space_flight)
+
+    http_response = sut.route(http_request=http_request)
+
+    print("mock", mock_one_space_flight)
+    print("usecase", use_case.param_insert_one_data)
+    print("status_code", http_response.status_code)
+    assert mock_one_space_flight == use_case.param_insert_one_data
+
+    assert http_response.status_code == 200
+    assert "error" not in http_response.body
+
+
+def test_route_error_no_body():
+    sut, use_case = make_sut()
+
+    http_request = HttpRequest()
+    http_response = sut.route(http_request=http_request)
+
+    assert use_case.param_insert_one_data is None
+
+    assert http_response.status_code == 400
+    assert "error" in http_response.body
